f8689c788c177291af914537f383ef2cd1e63ca3
Restyled.io <commits@restyled.io>
2023-04-23 19:12:25 +0000

    Restyled by clang-format

--- src/main/java/com/group7/model/Game.java
+++ src/main/java/com/group7/model/Game.java
@@ -1,252 +1,217 @@
 package com.group7.model;
 
-import com.group7.model.cards.Card;
 import com.group7.controller.Controller;
+import com.group7.model.cards.Card;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Game {
-    Controller m_control;
-    int m_numberOfPlayers;
-    ArrayList <Card> FuanaCards;
-    ArrayList <Integer> Scores;
-    ArrayList <Player> players;
-    Deck EarthDeck;
-    Deck IslandDeck;
-    Deck ClimateDeck;
-    Deck FuanaDeck;
-    Deck discardPile;
+  Controller m_control;
+  int m_numberOfPlayers;
+  ArrayList<Card> FuanaCards;
+  ArrayList<Integer> Scores;
+  ArrayList<Player> players;
+  Deck EarthDeck;
+  Deck IslandDeck;
+  Deck ClimateDeck;
+  Deck FuanaDeck;
+  Deck discardPile;
 
-    int turn = 0;
-    Player activePlayer;
-    
-    // For testing purposes
-    public Game(int numberOfPlayers)
-    {   
-        EarthDeck = new Deck();
-        IslandDeck = new Deck();
-        ClimateDeck = new Deck();
-        FuanaDeck = new Deck();
-        discardPile = new Deck();
-        FuanaCards = new ArrayList<Card>();
-        Scores = new ArrayList<Integer>();
-        players = new ArrayList<Player>();
-        m_numberOfPlayers = numberOfPlayers;
-        // Game Setup Phase
-        SetupPhase();
-        // if ( numboerOfPlayers == 1)
-        Iterator<Player> iter = players.iterator();
-        activePlayer = iter.next();
+  int turn = 0;
+  Player activePlayer;
 
-        // takeTurnZero(); // Island and Climate Setup
-        mainTurnLoop(iter); // Take a regular turn, and being the checks for complete Island
+  // For testing purposes
+  public Game(int numberOfPlayers) {
+    EarthDeck = new Deck();
+    IslandDeck = new Deck();
+    ClimateDeck = new Deck();
+    FuanaDeck = new Deck();
+    discardPile = new Deck();
+    FuanaCards = new ArrayList<Card>();
+    Scores = new ArrayList<Integer>();
+    players = new ArrayList<Player>();
+    m_numberOfPlayers = numberOfPlayers;
+    // Game Setup Phase
+    SetupPhase();
+    // if ( numboerOfPlayers == 1)
+    Iterator<Player> iter = players.iterator();
+    activePlayer = iter.next();
+
+    // takeTurnZero(); // Island and Climate Setup
+    mainTurnLoop(
+        iter); // Take a regular turn, and being the checks for complete Island
+  }
+
+  public Game(Controller controller, int numberOfPlayers) {
+    m_control = controller;
+    EarthDeck = new Deck();
+    IslandDeck = new Deck();
+    ClimateDeck = new Deck();
+    FuanaDeck = new Deck();
+    discardPile = new Deck();
+    FuanaCards = new ArrayList<Card>();
+    Scores = new ArrayList<Integer>();
+    players = new ArrayList<Player>();
+    m_numberOfPlayers = numberOfPlayers;
+    // Game Setup Phase
+    SetupPhase();
+    // if ( numboerOfPlayers == 1)
+    //         SetupGaia();
+
+    Iterator<Player> iters = players.iterator();
+    activePlayer = iters.next();
+    // takeTurnZero(); // Island and Climate Setup
+    mainTurnLoop(
+        iters); // Take a regular turn, and being the checks for complete Island
+  }
+
+  //! Assumeing Solo Game
+  private void SetupPhase() {
+    try {
+      EarthDeck.fillEarthDeck();
+      ClimateDeck.fillClimateDeck();
+      FuanaDeck.fillFuanaDeck();
+      IslandDeck.fillIslandDeck();
+    } catch (Exception e) {
+      System.out.println(e.toString());
+    }
+    for (int i = 0; i < m_numberOfPlayers; i++) {
+      players.add(new Player(this));
     }
 
-    
-    public Game(Controller controller, int numberOfPlayers)
-    {   
-        m_control = controller;
-        EarthDeck = new Deck();
-        IslandDeck = new Deck();
-        ClimateDeck = new Deck();
-        FuanaDeck = new Deck();
-        discardPile = new Deck();
-        FuanaCards = new ArrayList<Card>();
-        Scores = new ArrayList<Integer>();
-        players = new ArrayList<Player>();
-        m_numberOfPlayers = numberOfPlayers;
-        // Game Setup Phase
-        SetupPhase();
-        // if ( numboerOfPlayers == 1)
-        //         SetupGaia();
-
-        Iterator<Player> iters = players.iterator();
-        activePlayer = iters.next();
-        // takeTurnZero(); // Island and Climate Setup
-        mainTurnLoop(iters); // Take a regular turn, and being the checks for complete Island
+    for (int j = 0; j < 4; j++) {
+      FuanaCards.add(FuanaDeck.dealCard());
     }
-    
-    //! Assumeing Solo Game
-    private void SetupPhase()
-    {
-        try{
-            EarthDeck.fillEarthDeck();
-            ClimateDeck.fillClimateDeck();
-            FuanaDeck.fillFuanaDeck();
-            IslandDeck.fillIslandDeck();
+    // 1 Player game is standard expect everything to break
+    PlayerSetup();
+  }
+
+  private void PlayerSetup() {
+    for (int i = 0; i < m_numberOfPlayers; i++) {
+      Card island = IslandDeck.dealCard();
+      players.get(i).getHand().add(island);
+      // Activate the island immediate ability with black background
+      // E.g: island.parseAbility("black");
+      // island.parseAbilities(island.getM_abilities()) ;
+      // This should work if parseAbility is complete
+      players.get(i).getHand().add(ClimateDeck.dealCard());
+      // players.get(i).getHand().add(EcosystemDeck.dealCard()); // if we ever
+      // get here
+    }
+  }
+
+  void mainTurnLoop(Iterator<Player> iter) {
+    while (!isTableauDeckFilled()) {
+      if (!iter.hasNext()) {
+        iter = players.iterator();
+      }
+      // The current Player taking turn
+      String nextAction = activePlayer.takeTurn();
+      // Now iterate to the other player and perform inactive action
+      // The go back to the current player
+      for (int i = 0; i < this.players.size(); i++) {
+        if (!iter.hasNext()) {
+          iter = players.iterator();
         }
-        catch (Exception e)
-        {
-            System.out.println(e.toString());
-        }
-        for (int i = 0; i < m_numberOfPlayers; i++)
-        {
-            players.add(new Player(this));
-        }
-
-        for (int j = 0; j<4; j++)
-        {
-            FuanaCards.add(FuanaDeck.dealCard());
-        }
-        // 1 Player game is standard expect everything to break
-        PlayerSetup();
+        iter.next().takeInactiveAction(nextAction);
+      }
+      // Increment the turn and then iterate to the next player
+      turn++;
+      activePlayer = iter.next();
     }
-
-    
-    private void PlayerSetup()
-    {
-        for (int i = 0; i < m_numberOfPlayers; i++)
-        {
-            Card island = IslandDeck.dealCard();
-            players.get(i).getHand().add(island);
-            // Activate the island immediate ability with black background
-            // E.g: island.parseAbility("black");
-            // island.parseAbilities(island.getM_abilities()) ;
-            // This should work if parseAbility is complete
-            players.get(i).getHand().add(ClimateDeck.dealCard());
-            // players.get(i).getHand().add(EcosystemDeck.dealCard()); // if we ever get here
-        }
+  }
+  boolean isTableauDeckFilled() {
+    for (Player currentPlayer : players) {
+      if (currentPlayer.m_tableau.isBoardFilled()) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    void mainTurnLoop(Iterator<Player> iter) {
-        while (!isTableauDeckFilled()) {
-            if (!iter.hasNext()) {
-                iter = players.iterator();
-            }
-            // The current Player taking turn
-            String nextAction = activePlayer.takeTurn();
-            // Now iterate to the other player and perform inactive action
-            // The go back to the current player
-            for (int i = 0; i < this.players.size(); i++) {
-                if (!iter.hasNext()) {
-                    iter = players.iterator();
-                }
-                iter.next().takeInactiveAction(nextAction);
-            }
-            //Increment the turn and then iterate to the next player
-            turn++;
-            activePlayer = iter.next();
-        }
-    }
-    boolean isTableauDeckFilled() {
-        for (Player currentPlayer : players) {
-            if (currentPlayer.m_tableau.isBoardFilled()) {
-                return true;
-            }
-        }
-        return false;
-   }
+  String getActionChoice() { return m_control.getActionChoice(); }
 
-    String getActionChoice()
-    {
-        return m_control.getActionChoice();
-    }
-    
-    public ArrayList<Card> getFuanaCards()
-    {
-        return FuanaCards;
-    }
+  public ArrayList<Card> getFuanaCards() { return FuanaCards; }
 
-    public ArrayList<Player> getPlayers()
-    {
-        return players;
-    }
-    public Player getActivePlayer() {return activePlayer;}
-    void planting()
-    {
-        // Active Player Action
-        // Plant 2 Cards
-        // Draw 4 Discard 3 (not compost)
-        
-        // Gaia Action
-        // Your Discards move to compost
+  public ArrayList<Player> getPlayers() { return players; }
+  public Player getActivePlayer() { return activePlayer; }
+  void planting() {
+    // Active Player Action
+    // Plant 2 Cards
+    // Draw 4 Discard 3 (not compost)
 
-        // Trigger Green Abilities
-    }
+    // Gaia Action
+    // Your Discards move to compost
 
-    void compostion()
-    {
-        // Player Action
-        // Gain 5 Soil
-        // Compost 2 Cards from the EarthDeck
+    // Trigger Green Abilities
+  }
 
-        // Gaia Action
-        // Compost 2 cards, Compost 1 for every 2 soil gained this turn
+  void compostion() {
+    // Player Action
+    // Gain 5 Soil
+    // Compost 2 Cards from the EarthDeck
 
-        // Trugger Red and Multicolored Abilities
-    }
+    // Gaia Action
+    // Compost 2 cards, Compost 1 for every 2 soil gained this turn
 
-    void watering()
-    {
-        //Player Action
-        // Gain 6 sprouts
-        // May gain 2 soul
+    // Trugger Red and Multicolored Abilities
+  }
 
-        // Gaia Action
-        // you gain 1 sprout per red ability you have
+  void watering() {
+    // Player Action
+    //  Gain 6 sprouts
+    //  May gain 2 soul
 
-        // Trigger all blue and multicolored abilities
-    }
-    
-    void growing()
-    {
-        // Player Action
-        // Draw 4 to hand
-        // Place 2 growth
+    // Gaia Action
+    // you gain 1 sprout per red ability you have
 
-        // Gaia Action
-        // gain 1 growth for each draw this turn
+    // Trigger all blue and multicolored abilities
+  }
 
-        // Trigger Yellow and Multicolored Abilities
-    }
+  void growing() {
+    // Player Action
+    // Draw 4 to hand
+    // Place 2 growth
 
-    /**
-     * @return the climateDeck
-     */
-    public Deck getClimateDeck() {
-        return ClimateDeck;
-    }
+    // Gaia Action
+    // gain 1 growth for each draw this turn
 
-    /**
-     * @return the earthDeck
-     */
-    public Deck getEarthDeck() {
-        return EarthDeck;
-    }
+    // Trigger Yellow and Multicolored Abilities
+  }
 
-    /**
-     * @return the fuanaDeck
-     */
-    public Deck getFuanaDeck() {
-        return FuanaDeck;
-    }
+  /**
+   * @return the climateDeck
+   */
+  public Deck getClimateDeck() { return ClimateDeck; }
 
-    /**
-     * @return the islandDeck
-     */
-    public Deck getIslandDeck() {
-        return IslandDeck;
-    }
+  /**
+   * @return the earthDeck
+   */
+  public Deck getEarthDeck() { return EarthDeck; }
 
-    /**
-     * @return the scores
-     */
-    public ArrayList<Integer> getScores() {
-        return Scores;
-    }
+  /**
+   * @return the fuanaDeck
+   */
+  public Deck getFuanaDeck() { return FuanaDeck; }
 
-    /**
-     * @return the m_numberOfPlayers
-     */
-    public int getM_numberOfPlayers() {
-        return m_numberOfPlayers;
-    }
+  /**
+   * @return the islandDeck
+   */
+  public Deck getIslandDeck() { return IslandDeck; }
 
-    /**
-     * @return the discardPile
-     */
-    public Deck getDiscardPile() {
-        return discardPile;
-    }
-    Controller getController() { return m_control; }
+  /**
+   * @return the scores
+   */
+  public ArrayList<Integer> getScores() { return Scores; }
+
+  /**
+   * @return the m_numberOfPlayers
+   */
+  public int getM_numberOfPlayers() { return m_numberOfPlayers; }
+
+  /**
+   * @return the discardPile
+   */
+  public Deck getDiscardPile() { return discardPile; }
+  Controller getController() { return m_control; }
 }
--- src/main/java/com/group7/model/Player.java
+++ src/main/java/com/group7/model/Player.java
@@ -1,340 +1,272 @@
 package com.group7.model;
 
+import com.group7.model.board.Tableau;
+import com.group7.model.cards.AbilityPair;
+import com.group7.model.cards.Card;
+import com.group7.model.cards.EarthCard;
 import java.util.ArrayList;
 import java.util.Stack;
 
-import com.group7.model.board.Tableau;
-import com.group7.model.cards.Card;
-import com.group7.model.cards.EarthCard;
-import com.group7.model.cards.AbilityPair;
-
-
 public class Player {
-    String playerName = "John Smith";
-    
-    int soil;
-    int gainedSoil = 0;
-    int gainedCards = 0;
-    int trunks = 0;
-    int sprouts = 0;
+  String playerName = "John Smith";
 
-    Card m_islandCard, m_climateCard;
-    ArrayList <Card> hand;
-    ArrayList <Card> compostPile;
-    ArrayList <Card> discardPile;
-    ArrayList <Card> eventStack;
-    Tableau playerTableau;
-    Game m_game;
-    Tableau m_tableau;
+  int soil;
+  int gainedSoil = 0;
+  int gainedCards = 0;
+  int trunks = 0;
+  int sprouts = 0;
 
+  Card m_islandCard, m_climateCard;
+  ArrayList<Card> hand;
+  ArrayList<Card> compostPile;
+  ArrayList<Card> discardPile;
+  ArrayList<Card> eventStack;
+  Tableau playerTableau;
+  Game m_game;
+  Tableau m_tableau;
 
-    public String getName()
-    {
-        return playerName;
-    }
-    Player(Game currentGame)
-    {
-        hand = new ArrayList<>();
-        compostPile = new ArrayList<>();
-        discardPile = new ArrayList<>();
-        eventStack = new ArrayList<>();
-        m_game = currentGame;
-        m_tableau = new Tableau();
-        playerTableau = new Tableau();
-    }
-    public ArrayList <Card> getHand()
-    {
-        return hand;
-    }
-    void setIsland(Card islandCard)
-    {
-        m_islandCard = islandCard;
-    }
-    void setClimate(Card cliamteCard)
-    {
-        m_climateCard = cliamteCard;
+  public String getName() { return playerName; }
+  Player(Game currentGame) {
+    hand = new ArrayList<>();
+    compostPile = new ArrayList<>();
+    discardPile = new ArrayList<>();
+    eventStack = new ArrayList<>();
+    m_game = currentGame;
+    m_tableau = new Tableau();
+    playerTableau = new Tableau();
+  }
+  public ArrayList<Card> getHand() { return hand; }
+  void setIsland(Card islandCard) { m_islandCard = islandCard; }
+  void setClimate(Card cliamteCard) { m_climateCard = cliamteCard; }
+
+  void placeCardontoTableau(int row, int collumn, Card cardDrawn) {
+    playerTableau.setCard(row, collumn, cardDrawn);
+  }
+
+  public Boolean isBoardFilled() { return playerTableau.isBoardFilled(); }
+  String takeTurn() {
+    String actionChosen = "";
+
+    // Choose an action
+    //  Switch to correct branch
+    actionChosen = selectAction();
+
+    switch (actionChosen) {
+    case "planting":
+      activePlanting();
+      break;
+    case "composting":
+      activeComposting();
+      break;
+    case "growing":
+      activeGrowing();
+      break;
+    case "watering":
+      activeWatering();
+      break;
     }
 
-    void placeCardontoTableau(int row, int collumn, Card cardDrawn) {
-        playerTableau.setCard(row, collumn, cardDrawn);
+    return actionChosen;
+  }
+
+  String selectAction() {
+    String action; // = "planting";
+    action = m_game.getActionChoice();
+    return action;
+  }
+
+  void activePlanting() {
+    // Active Player Action
+    // Plant 2 Cards
+    plant();
+    plant();
+    // Draw 4 Discard 3 (not compost)
+    // tableau.plant(Card, Card);
+    for (int i = 0; i < 4; i++)
+      hand.add(m_game.EarthDeck.dealCard());
+    discard(3);
+  }
+
+  void takeInactiveAction(String inactiveAction) {
+    switch (inactiveAction) {
+    case "planting":
+      inactivePlanting();
+      break;
+    case "composting":
+      inactiveComposting();
+      break;
+    case "growing":
+      inactiveGrowing();
+      break;
+    case "watering":
+      inactiveWatering();
+      break;
     }
+  }
 
-    public Boolean isBoardFilled(){
-        return playerTableau.isBoardFilled();
+  void inactivePlanting() {
+    // Gaia Action
+    // discarded cards become compost
+    for (int i = 0; i < 3; i++) {
+      compostPile.add(discardPile.get(discardPile.size()));
+      compostPile.remove(discardPile.size());
     }
-    String takeTurn()
-    {
-        String actionChosen = "";
+  }
 
-        //Choose an action
-        // Switch to correct branch
-        actionChosen = selectAction();
+  void plant() {
+    Card cardToPlant;
 
-        switch (actionChosen)
-        {
-            case "planting":
-                activePlanting(); break;
-            case "composting":
-                activeComposting(); break;
-            case "growing":
-                activeGrowing(); break;
-            case "watering":
-                activeWatering(); break;
+    String message = "Select a card to plant.";
+    // push message to GUI along with input for a number
+    // Select a card from Hand
+
+    // cardToPlant = m_game.m_control.getCardChoice();
+    int x = 0, y = 0;
+
+    // Place it in the Tabelu anywhere on first planting
+    // only adjacent to existing cards after.
+
+    // m_tableau.setCard(x, y, cardToPlant);
+
+    // cardToPlant.get_abilities();
+  }
+
+  private int placeInIsland(Card card) {
+    // Get a specific Tile number from tui
+    // m_game.m_control.getIslandCoords();
+
+    //
+
+    return 0;
+  }
+
+  void activeComposting() {
+    // The active player gains five soil
+    //  setGainedSoil(2);
+    //  They also and adds two cards from the deck to their
+    // compost pile
+    //        compostPile.add
+  }
+
+  void inactiveComposting() {
+    // Gain two soil or compost two card
+  }
+
+  void activeWatering() {
+    // Gain two sprout or two soil
+  }
+
+  void inactiveWatering() {
+    // Gain two sprout or two soil
+  }
+
+  void activeGrowing() {
+    // Draw four card from the pile and add two growth token cards
+  }
+
+  void inactiveGrowing() {
+    // Draw two card or gain two growth
+  }
+
+  // example ability section:
+  // Yellow:+1 Trunks +1 Sprouts
+  void abilityParser(
+      AbilityPair ability) // temporary location for the ability parser function
+  {
+    int num = 0;
+    String number;
+    String ability_text = ability.getText();
+    String[] ability_split = ability_text.split(" ");
+    for (String word : ability_split) {
+      if (word.contains("then")) {
+        // skip or stack maybe not sure if we need to stack abilities
+        // like this in this context
+      }
+      if (word.contains("+") || word.contains("-")) {
+        number = word.substring(1);
+        num = Integer.valueOf(number);
+      } else if (word.contains("Trunks")) {
+        trunks += num;
+      } else if (word.contains("Sprouts")) {
+        sprouts += num;
+      } else if (word.contains("Soil") && num < 0) {
+        soil += num;
+      } else if (word.contains("Soil")) {
+        soil += num;
+        gainedSoil += num;
+        // Soil gained need to be reset somewhere putting at the end of the turn
+        // for now
+      } else if (word.contains("Cards")) {
+        for (int y = 0; y < num; y++) {
+          hand.add(m_game.EarthDeck.dealCard());
         }
-
-        return actionChosen;
+      } else if (word.contains("Compost") && num < 0) {
+        // Move top most compost card to discard pile
+      } else if (word.contains("Compost")) {
+        // Move a card to the compost pile
+      }
     }
-    
-    String selectAction()
-    {
-        String action;// = "planting";
-        action = m_game.getActionChoice();
-        return action;
+  }
+
+  /**
+   * @return the compostPile
+   */
+  public ArrayList<Card> getCompostPile() { return compostPile; }
+
+  /**
+   * @return the gainedSoil
+   */
+  public int getGainedSoil() { return gainedSoil; }
+
+  /**
+   * @return the eventStack
+   */
+  public ArrayList<Card> getEventStack() { return eventStack; }
+
+  /**
+   * @return the discardPile
+   */
+  public ArrayList<Card> getDiscardPile() { return discardPile; }
+
+  /**
+   * @return the gainedCards
+   */
+  public int getGainedCards() { return gainedCards; }
+
+  /**
+   * @return the m_climateCard
+   */
+  public Card getM_climateCard() { return m_climateCard; }
+
+  /**
+   * @return the m_islandCard
+   */
+  public Card getM_islandCard() { return m_islandCard; }
+
+  /**
+   * @return the playerTabulue
+   */
+  public Tableau getPlayerTableau() { return playerTableau; }
+
+  /**
+   * @return the soil
+   */
+  public int getSoil() { return soil; }
+
+  // Public for testing purposes
+  public void discard(int numberToDiscard) {
+    for (int i = 0; i < numberToDiscard; i++) {
+      if (hand.size() == 0) {
+        return;
+      } // No discard possible, should never reach this from
+      // the way the game is designed but you never know so its here.
+      Card someCardToDiscard;
+      someCardToDiscard = m_game.getController().getCardChoice();
+      // someCard = getHand().get(0); // TODO way to select a card from the hand
+      hand.remove(someCardToDiscard);
     }
-
-    void activePlanting()
-    {
-        // Active Player Action
-        // Plant 2 Cards
-        plant();
-        plant();
-        // Draw 4 Discard 3 (not compost)
-        // tableau.plant(Card, Card);
-        for (int i = 0; i < 4; i++)
-            hand.add(m_game.EarthDeck.dealCard());
-        discard(3);
-    }
-
-    void takeInactiveAction(String inactiveAction)
-    {
-        switch (inactiveAction)
-        {
-            case "planting":
-                inactivePlanting(); break;
-            case "composting":
-                inactiveComposting(); break;
-            case "growing":
-                inactiveGrowing(); break;
-            case "watering":
-                inactiveWatering(); break;
-        }
-    }
-    
-    void inactivePlanting()
-    {
-        // Gaia Action
-        // discarded cards become compost
-        for (int i = 0; i < 3; i ++)
-        {
-            compostPile.add(discardPile.get(discardPile.size()));
-            compostPile.remove(discardPile.size());
-        }
-    }
-
-    void plant()
-    {
-        Card cardToPlant;
-
-        String message = "Select a card to plant.";
-        // push message to GUI along with input for a number
-        // Select a card from Hand
-
-        // cardToPlant = m_game.m_control.getCardChoice();
-        int x = 0,y = 0;
-        
-        // Place it in the Tabelu anywhere on first planting
-        // only adjacent to existing cards after.
-
-        // m_tableau.setCard(x, y, cardToPlant);
-        
-        // cardToPlant.get_abilities();
-
-    }
-
-    private int placeInIsland(Card card)
-    {
-        // Get a specific Tile number from tui
-        // m_game.m_control.getIslandCoords();
-
-        // 
-
-        return 0;
-    }
-
-
-    void activeComposting()
-    {
-        //The active player gains five soil
-        // setGainedSoil(2);
-        // They also and adds two cards from the deck to their
-        //compost pile
-//        compostPile.add
-    }
-
-    void inactiveComposting()
-    {
-        // Gain two soil or compost two card
-
-    }
-
-    void activeWatering()
-    {
-        // Gain two sprout or two soil
-
-    }
-    
-    void inactiveWatering()
-    {
-        // Gain two sprout or two soil
-
-    }
-
-    void activeGrowing()
-    {
-        // Draw four card from the pile and add two growth token cards
-
-    }
-
-    void inactiveGrowing()
-    {
-        // Draw two card or gain two growth
-
-    }
-
-
-    // example ability section: 
-    // Yellow:+1 Trunks +1 Sprouts
-    void abilityParser(AbilityPair ability) // temporary location for the ability parser function
-    {
-        int num = 0;
-        String number;
-        String ability_text = ability.getText();
-        String [] ability_split = ability_text.split(" ");
-        for ( String word : ability_split )
-        {
-            if ( word.contains("then") )
-            {
-                // skip or stack maybe not sure if we need to stack abilities
-                // like this in this context
-            }
-            if (word.contains("+") || word.contains("-"))
-            {
-                number = word.substring(1);
-                num = Integer.valueOf(number);
-            }
-            else if ( word.contains("Trunks") )
-            {
-                trunks += num;
-            }
-            else if ( word.contains("Sprouts") )
-            {
-                sprouts += num;
-            }
-            else if ( word.contains("Soil") && num < 0)
-            {
-                soil += num;
-            }
-            else if ( word.contains("Soil") )
-            {
-                soil += num;
-                gainedSoil += num; 
-                // Soil gained need to be reset somewhere putting at the end of the turn for now
-            }
-            else if ( word.contains("Cards") )
-            {
-                for (int y = 0; y < num; y++)
-                {
-                    hand.add(m_game.EarthDeck.dealCard());
-                }
-            }
-            else if ( word.contains("Compost") && num < 0)
-            {
-                // Move top most compost card to discard pile
-            }
-            else if ( word.contains("Compost") )
-            {
-                // Move a card to the compost pile
-            }
-        }
-    }
-
-    /**
-     * @return the compostPile
-     */
-    public ArrayList<Card> getCompostPile() {
-        return compostPile;
-    }
-
-    /**
-     * @return the gainedSoil
-     */
-    public int getGainedSoil() {
-        return gainedSoil;
-    }
-
-    /**
-     * @return the eventStack
-     */
-    public ArrayList<Card> getEventStack() {
-        return eventStack;
-    }
-
-    /**
-     * @return the discardPile
-     */
-    public ArrayList<Card> getDiscardPile() {
-        return discardPile;
-    }
-
-    /**
-     * @return the gainedCards
-     */
-    public int getGainedCards() {
-        return gainedCards;
-    }
-
-    /**
-     * @return the m_climateCard
-     */
-    public Card getM_climateCard() {
-        return m_climateCard;
-    }
-
-    /**
-     * @return the m_islandCard
-     */
-    public Card getM_islandCard() {
-        return m_islandCard;
-    }
-
-    /**
-     * @return the playerTabulue
-     */
-    public Tableau getPlayerTableau() {
-        return playerTableau;
-    }
-
-    /**
-     * @return the soil
-     */
-    public int getSoil() {
-        return soil;
-    }
-
-    // Public for testing purposes
-    public void discard(int numberToDiscard)
-    {
-        for ( int i = 0; i < numberToDiscard; i++)
-        {
-            if (hand.size() == 0) { return; } // No discard possible, should never reach this from 
-            // the way the game is designed but you never know so its here.
-            Card someCardToDiscard;
-            someCardToDiscard = m_game.getController().getCardChoice();
-            // someCard = getHand().get(0); // TODO way to select a card from the hand
-            hand.remove(someCardToDiscard);
-        }
-    }
-    void setGainedSoil(int amount) { soil+= amount; }
-    void setGainedTrunk(int amount) { trunks += amount; }
-    void setGainedSprout(int amount) { sprouts += amount; }
+  }
+  void setGainedSoil(int amount) { soil += amount; }
+  void setGainedTrunk(int amount) { trunks += amount; }
+  void setGainedSprout(int amount) { sprouts += amount; }
 }
